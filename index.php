20b2c69e2314dd767bcb0a466639c2b603b7d0d3
liu-mojia <liumojia.mj@gmail.com>
2023-11-01 01:46:32 +0800

    link cart

--- index.php
+++ index.php
@@ -42,11 +42,12 @@ $username = $_SESSION['username'];
         } ?>
         
 
-        <a>
+        <a href="pages/cart.php">
           <img src="./resource/cartIcon.svg" height="26px" width="26px" />
         </a>
       </div>
     </div>
+    
 
     <div class="content">
       <div class="left-column">
