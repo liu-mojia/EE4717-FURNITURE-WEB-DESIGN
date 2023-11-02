8a8a9c951500ec9d364c5fc7bc04572d4e6de997
LYONG005 <78222547+LYONG005@users.noreply.github.com>
2023-11-02 20:50:55 +0800

    purchase now function end-to-end implemented for backend

--- pages/delivery.php
+++ pages/delivery.php
@@ -8,6 +8,7 @@ if (isset($_SESSION['email'])) {
     $userEmail = "";
 }
 
+//Purchase Now Selected
 if (!isset($_SESSION['items'])) {
     $name = $_SESSION['buyNow'][0];
     $quantity = $_SESSION['buyNow'][1];
@@ -47,14 +48,14 @@ if (!isset($_SESSION['items'])) {
       <h3 style="padding: 100px 0 0 0">Delivery Details</h3>
       <form method="post" action="../php/deliveryPost.php" id="form">
         <table id="delivery-table">
-          <tr><td><label for="name">Name</label></td></tr>
-          <tr><td><input type="text" class="text-box" id="name" name="name"></td></tr>
-          <tr><td><label for="email">Email</label></td></tr>
-          <tr><td><input type="email" class="text-box" id="email" name="email" value="<?php echo $userEmail?>"></td></tr>
-          <tr><td><label for="address">Address</label></td></tr>
-          <tr><td><input type="text" class="text-box" id="address" name="address"></td></tr>
-          <tr><td><label for="phoneNo">Phone Number</label></td></tr>
-          <tr><td><input type="text" class="text-box" id="phoneNo" name="phoneNo"></td></tr>
+          <tr><td><label for="name">Name*</label></td></tr>
+          <tr><td><input type="text" class="text-box" id="name" name="name" required></td></tr>
+          <tr><td><label for="email">Email*</label></td></tr>
+          <tr><td><input type="email" class="text-box" id="email" name="email" value="<?php echo $userEmail?>" required></td></tr>
+          <tr><td><label for="address">Address*</label></td></tr>
+          <tr><td><input type="text" class="text-box" id="address" name="address" required></td></tr>
+          <tr><td><label for="phoneNo">Phone Number*</label></td></tr>
+          <tr><td><input type="text" class="text-box" id="phoneNo" name="phoneNo" required></td></tr>
         </table>
         <p style="padding: 0 0 0 250px">
           <input type="submit" style="padding: 20px 20px 20px 20px;" value="Next">
@@ -64,8 +65,11 @@ if (!isset($_SESSION['items'])) {
 
     <div class="right-column">
       <h3 style="padding: 90px 0 0 0">Order Summary</h3>
-      <?php echo "<p>$name($quantity) $$price</p>" ?>
-        <p>Delivery </p>
+      <?php
+        echo "<p>$name($quantity) $".number_format($price, 2)."</p>";
+        echo "<p>Delivery $10.00</p>";
+        echo "<p>Total $".number_format($price + 10, 2)."</p>";
+        ?>
     </div>
   </div>
 </body>
